1639592246d62566d595bf60d938fd3a48ee5890
jamesjutt <jamesrjutt@gmail.com>
2019-07-04 12:27:58 -0400

    practice CRUD

--- mongodb.js
+++ mongodb.js
@@ -1,24 +1,36 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log("Unable to connect to database!");
-    }
-
-    const db = client.db(databaseName);
-
-    db.collection("users").insertOne({
-      name: "James",
-      age: 25
-    });
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log("Unable to connect to database!");
   }
-);
+
+  const db = client.db(databaseName);
+
+  db.collection("users")
+    .deleteMany({
+      age: 25
+    })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  db.collection("task")
+    .deleteOne({
+      description: "This is the first insert"
+    })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
